be2b01aa8636adda51482a4e9601c169c1b6b699
Minh <minheragon12345@gmail.com>
2025-02-11 22:10:55 +0700

    README & build.gradle changes

--- common/src/main/java/io/github/manasmods/template/ExampleMod.java
+++
@@ -1,9 +0,0 @@
-package io.github.manasmods.template;
-
-public final class ExampleMod {
-    public static final String MOD_ID = "manas_template";
-
-    public static void init() {
-        // Write common init code here.
-    }
-}
